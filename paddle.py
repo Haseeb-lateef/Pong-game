99633381b7a8943922408b84a17641bd9270f3b0
Haseeb-090 <C24435332@myTUDublin.ie>
2026-01-28 15:14:15 +0000

    created starting screen

---
+++ paddle.py
@@ -0,0 +1,26 @@
+from turtle import Turtle
+
+
+class Paddle(Turtle):
+
+    def __init__(self, start_pos):
+        super().__init__()
+        self.start_pos = start_pos
+        self.penup()
+        self.goto(start_pos)
+        self.shape("square")
+        self.shapesize(5,1)
+        self.color("white")
+
+    def up(self):
+        y= self.ycor() +20
+        self.goto(self.xcor(),y)
+
+    def down(self):
+        y= self.ycor() -20
+        self.goto(self.xcor(),y)
+
+
+
+
+
